9453004ef4a6b2980ace402c52f2b1378cead2a9
Nitesh Daga <niteshcooldaga@gmail.com>
2020-07-01 16:25:08 +0530

    Initial Setup

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
+  setupFiles: ['./tests/jest-setup.js'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(vuetify)/)',
+  ],
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
 }
